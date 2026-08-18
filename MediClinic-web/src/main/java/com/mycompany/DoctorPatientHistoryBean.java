9ed129beb79d806a52dd336bb3939ee2e4e4a1be
acefinfo <fromaashutoshthapa@gmail.com>
2026-08-18 10:34:05 +0545

    fix: Patient history fixed

--- MediClinic-web/src/main/java/com/mycompany/DoctorPatientHistoryBean.java
+++ MediClinic-web/src/main/java/com/mycompany/DoctorPatientHistoryBean.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -77,7 +76,6 @@ public class DoctorPatientHistoryBean implements Serializable {
      * Loads the patient's profile and full consultation/prescription
      * history.
      */
-    @PostConstruct
     public void load() {
         if (patientId == null) {
             accessDenied = true;
